a23f3f0470c6c7965860dd5afd66edd31a9444b2
Gediminas Paulauskas <gediminas@idega.com>
2006-10-10 15:47:48 +0000

    - use new Chiba XSLTTransformer interface, take TransformerService initialized in bundle starter- use FormBean to load the form- rename uri to resourcePath

--- src/java/com/idega/block/form/presentation/FormViewer.java
+++ src/java/com/idega/block/form/presentation/FormViewer.java
@@ -1,5 +1,5 @@
 /*
- * $Id: FormViewer.java,v 1.2 2006/09/27 10:01:44 gediminas Exp $ Created on Aug
+ * $Id: FormViewer.java,v 1.3 2006/10/10 15:47:48 gediminas Exp $ Created on Aug
  * 17, 2006
  * 
  * Copyright (C) 2006 Idega Software hf. All Rights Reserved.
@@ -10,57 +10,60 @@
 package com.idega.block.form.presentation;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.rmi.RemoteException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
+import org.chiba.adapter.ui.XSLTGenerator;
 import org.chiba.xml.xforms.ChibaBean;
 import org.chiba.xml.xforms.exception.XFormsException;
-import com.idega.block.form.business.FBXSLTGenerator;
-import com.idega.block.form.business.TransformerCache;
-import com.idega.business.IBOLookup;
-import com.idega.business.IBOLookupException;
-import com.idega.idegaweb.IWUserContext;
+import org.chiba.xml.xslt.TransformerService;
+import org.w3c.dom.Document;
+import com.idega.block.form.IWBundleStarter;
+import com.idega.block.form.bean.FormBean;
+import com.idega.content.bean.ManagedContentBeans;
 import com.idega.presentation.IWBaseComponent;
-import com.idega.presentation.IWContext;
-import com.idega.slide.business.IWSlideSession;
+import com.idega.webface.WFUtil;
 
 /**
  * 
- * Last modified: $Date: 2006/09/27 10:01:44 $ by $Author: gediminas $
+ * Last modified: $Date: 2006/10/10 15:47:48 $ by $Author: gediminas $
  * 
  * @author <a href="mailto:gediminas@idega.com">gediminas</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
-public class FormViewer extends IWBaseComponent {
+public class FormViewer extends IWBaseComponent implements ManagedContentBeans {
 
 	private static final Logger log = Logger.getLogger(FormViewer.class.getName());
 
 	private static final String BUNDLE_IDENTIFIER = "com.idega.block.form";
-	//private static String RENDERER_TYPE = "xforms";
-	
+
+	// private static String RENDERER_TYPE = "xforms";
 	private static final URI XSLT_URI = URI.create("bundle://" + BUNDLE_IDENTIFIER + "/resources/xslt/html4.xsl");
 
 	private ChibaBean chiba;
-	
-	private String uri;
-	private FBXSLTGenerator uiGenerator;
+
+	private XSLTGenerator uiGenerator;
+
+	private String resourcePath;
 
 	public FormViewer() {
 		super();
 	}
-	
+
 	public String getRendererType() {
 		return null;
-		//return RENDERER_TYPE;
+		// return RENDERER_TYPE;
 	}
 
 	@Override
 	protected void initializeComponent(FacesContext context) {
-		uiGenerator = new FBXSLTGenerator(TransformerCache.getInstance(context));
+		super.initializeComponent(context);
+		TransformerService transformerService = (TransformerService) getIWMainApplication(context).getAttribute(IWBundleStarter.TRANSFORMER_SERVICE);
+		uiGenerator = new XSLTGenerator();
+		uiGenerator.setTransformerService(transformerService);
 		uiGenerator.setStylesheetURI(XSLT_URI);
 	}
 
@@ -68,15 +71,20 @@ public class FormViewer extends IWBaseComponent {
 	public void encodeEnd(FacesContext ctx) throws IOException {
 		ResponseWriter out = ctx.getResponseWriter();
 		
-		if (uri == null) {
+		FormBean form = (FormBean) WFUtil.getBeanInstance(FORM_ITEM_BEAN_ID);
+		
+		if (form.getResourcePath() == null) {
+			log.fine("form.resourcePath is null");
+			form.setResourcePath(getResourcePath());			
 			super.encodeEnd(ctx);
 			return;
 		}
-
 		try {
-			InputStream stream = getIWSlideSession().getInputStream(uri);
+			form.load();
+			Document doc = form.getDocument();
+			
 			chiba = new ChibaBean();
-			chiba.setXMLContainer(stream);
+			chiba.setXMLContainer(doc);
 			chiba.init();
 		}
 		catch (RemoteException e) {
@@ -91,36 +99,36 @@ public class FormViewer extends IWBaseComponent {
 			log.log(Level.WARNING, "Could not set XML container", e);
 			return;
 		}
-				
 		try {
-			uiGenerator.setInputNode(chiba.getXMLContainer());
+			uiGenerator.setInput(chiba.getXMLContainer());
 			uiGenerator.setOutput(out);
 			uiGenerator.generate();
 		}
 		catch (XFormsException e) {
 			log.log(Level.WARNING, "Could not generate HTML from XForms document", e);
 		}
-
 		super.encodeEnd(ctx);
 	}
 
-	protected IWSlideSession getIWSlideSession() {
-		IWUserContext iwuc = IWContext.getInstance();
-		IWSlideSession session = null;
-		try {
-			session = (IWSlideSession) IBOLookup.getSessionInstance(iwuc, IWSlideSession.class);
-		}
-		catch (IBOLookupException e) {
-			log.log(Level.SEVERE, "Error getting Slide session", e);
-		}
-		return session;
-	}
-	
-    public String getUri() {
-		return uri;
+	public String getResourcePath() {
+		return resourcePath;
 	}
 
-	public void setUri(String uri) {
-		this.uri = uri;
+	public void setResourcePath(String resourcePath) {
+		this.resourcePath = resourcePath;
 	}
+
+	public Object saveState(FacesContext ctx) {
+		Object values[] = new Object[2];
+		values[0] = super.saveState(ctx);
+		values[1] = this.resourcePath;
+		return values;
+	}
+
+	public void restoreState(FacesContext ctx, Object state) {
+		Object values[] = (Object[]) state;
+		super.restoreState(ctx, values[0]);
+		this.resourcePath = (String) values[1];
+	}
+
 }
