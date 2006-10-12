14c4e29cf135cc6d2eeca4b2f9cfda3691396bc2
Gediminas Paulauskas <gediminas@idega.com>
2006-10-12 15:59:43 +0000

    implement a simple list, not using ContentListViewer

--- src/java/com/idega/block/form/presentation/FormListViewer.java
+++ src/java/com/idega/block/form/presentation/FormListViewer.java
@@ -1,5 +1,5 @@
 /*
- * $Id: FormListViewer.java,v 1.1 2006/10/10 15:58:11 gediminas Exp $ Created on
+ * $Id: FormListViewer.java,v 1.2 2006/10/12 15:59:43 gediminas Exp $ Created on
  * 24.1.2005
  * 
  * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
@@ -9,30 +9,96 @@
  */
 package com.idega.block.form.presentation;
 
-import com.idega.content.presentation.ContentItemListViewer;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.context.FacesContext;
+import org.apache.webdav.lib.WebdavResource;
+import org.apache.webdav.lib.WebdavResources;
+import com.idega.block.form.bean.FormBean;
+import com.idega.business.IBOLookup;
+import com.idega.presentation.IWBaseComponent;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.text.Link;
+import com.idega.presentation.text.ListItem;
+import com.idega.presentation.text.Lists;
+import com.idega.slide.business.IWSlideSession;
 
 /**
  * <p>
  * Displays a list of available XForms
  * </p>
  * 
- * Last modified: $Date: 2006/10/10 15:58:11 $ by $Author: gediminas $
+ * Last modified: $Date: 2006/10/12 15:59:43 $ by $Author: gediminas $
  * 
  * @author <a href="mailto:gediminas@idega.com">Gediminas Paulauskas</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
-public class FormListViewer extends ContentItemListViewer {
+public class FormListViewer extends IWBaseComponent {
 
-	final static String FORM_LIST_BEAN = "formListBean";
-
-	public FormListViewer() {
-		super();
-		setBeanIdentifier(FORM_LIST_BEAN);
-		setBaseFolderPath("/files/xforms");
-	}
+	private static final Logger log = Logger.getLogger(FormListViewer.class.getName());
+	private static final String FORMS_PATH = "/files/forms";
 	
-	protected String[] getToolbarActions(){
-		return new String[0];
+	private String detailsViewerPath = "#";
+	
+	/* (non-Javadoc)
+	 * @see com.idega.presentation.IWBaseComponent#initializeComponent(javax.faces.context.FacesContext)
+	 */
+	protected void initializeComponent(FacesContext context) {
+		Lists ul = new Lists();
+		
+		List<FormBean> forms = getForms();
+		for (FormBean f : forms) {
+			ListItem li = new ListItem();
+			Link a = new Link(f.getName());
+			a.setURL(getDetailsViewerPath());
+			a.addParameter("resourcePath", f.getResourcePath());
+			li.add(a);
+			ul.add(li);
+		}
+		
+		getChildren().add(ul);
+	}
+
+	public List<FormBean> getForms() {
+		List<FormBean> list = new ArrayList();
+		try {			
+			IWContext iwc = IWContext.getInstance();
+			IWSlideSession session = (IWSlideSession) IBOLookup.getSessionInstance(iwc, IWSlideSession.class);
+			WebdavResource root = session.getWebdavResource(FORMS_PATH);
+			for (WebdavResource folder : root.listWebdavResources()) {
+				if (folder.isCollection()) {
+					String formId = folder.getDisplayName();
+					WebdavResources rs = folder.getChildResources();
+					WebdavResource r = rs.getResource(folder.getName() + "/" + formId + ".xforms");
+					if (r == null) {
+						continue;
+					}
+					
+					// need to load for title
+					FormBean form = new FormBean();
+					form.setResourcePath(r.getPath());
+					form.load();
+					
+					list.add(form);
+				}
+			}
+		}
+		catch (IOException e) {
+			log.log(Level.WARNING, "Error listing forms", e);
+		}
+		return list;
+	}
+
+	
+	public String getDetailsViewerPath() {
+		return this.detailsViewerPath;
+	}
+
+	public void setDetailsViewerPath(String detailsViewerPath) {
+		this.detailsViewerPath = detailsViewerPath;
 	}
 
 }
