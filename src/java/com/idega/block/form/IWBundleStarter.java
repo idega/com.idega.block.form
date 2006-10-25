5b3e046bfc82bf89cd49c4143711d382749201a6
Gediminas Paulauskas <gediminas@idega.com>
2006-10-25 13:30:07 +0000

    cache default xslt at start

--- src/java/com/idega/block/form/IWBundleStarter.java
+++ src/java/com/idega/block/form/IWBundleStarter.java
@@ -1,5 +1,5 @@
 /**
- * $Id: IWBundleStarter.java,v 1.1 2006/10/10 15:29:16 gediminas Exp $
+ * $Id: IWBundleStarter.java,v 1.2 2006/10/25 13:30:07 gediminas Exp $
  * Created in 2006 by gediminas
  * 
  * Copyright (C) 2000-2006 Idega Software hf. All Rights Reserved.
@@ -9,6 +9,10 @@
  */
 package com.idega.block.form;
 
+import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.transform.TransformerException;
 import org.chiba.xml.xslt.TransformerService;
 import org.chiba.xml.xslt.impl.CachingTransformerService;
 import org.chiba.xml.xslt.impl.ResourceResolver;
@@ -22,18 +26,22 @@ import com.idega.idegaweb.include.GlobalIncludeManager;
  * <p>
  * TODO gediminas Describe Type IWBundleStarter
  * </p>
- * Last modified: $Date: 2006/10/10 15:29:16 $ by $Author: gediminas $
+ * Last modified: $Date: 2006/10/25 13:30:07 $ by $Author: gediminas $
  * 
  * @author <a href="mailto:gediminas@idega.com">Gediminas Paulauskas</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class IWBundleStarter implements IWBundleStartable {
+	
+	private static final Logger log = Logger.getLogger(IWBundleStarter.class.getName());
 
 	private static final String STYLE_SHEET_URL = "/style/xforms.css";
 
 	public static final String BUNDLE_IDENTIFIER = "com.idega.block.form";
 
-	public static final String TRANSFORMER_SERVICE = "transformer_service";
+	public static final String TRANSFORMER_SERVICE = TransformerService.class.getName();
+
+	public static final URI XSLT_URI = URI.create("bundle://" + BUNDLE_IDENTIFIER + "/resources/xslt/html4.xsl");
 
 	/*
 	 * (non-Javadoc)
@@ -47,6 +55,14 @@ public class IWBundleStarter implements IWBundleStartable {
 		ResourceResolver resolver = new BundleResourceResolver(application);
 		TransformerService transformerService = new CachingTransformerService(resolver);
 		application.setAttribute(TRANSFORMER_SERVICE, transformerService);
+
+        try {
+        	// cache default stylesheet
+			transformerService.getTransformer(XSLT_URI);
+		}
+		catch (TransformerException e) {
+			log.log(Level.SEVERE, "Cannot load XForms transformer stylesheet", e);
+		}
 	}
 
 	/*
