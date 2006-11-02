449187171c47cbed21154ddb230d0ccaedf91f09
Gediminas Paulauskas <gediminas@idega.com>
2006-11-02 14:40:41 +0000

    initialize XformsSessionManager from our config file

--- src/java/com/idega/block/form/IWBundleStarter.java
+++ src/java/com/idega/block/form/IWBundleStarter.java
@@ -1,5 +1,5 @@
 /**
- * $Id: IWBundleStarter.java,v 1.2 2006/10/25 13:30:07 gediminas Exp $
+ * $Id: IWBundleStarter.java,v 1.3 2006/11/02 14:40:41 gediminas Exp $
  * Created in 2006 by gediminas
  * 
  * Copyright (C) 2000-2006 Idega Software hf. All Rights Reserved.
@@ -9,10 +9,15 @@
  */
 package com.idega.block.form;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.transform.TransformerException;
+import org.chiba.web.session.impl.DefaultXFormsSessionManagerImpl;
+import org.chiba.xml.xforms.config.Config;
+import org.chiba.xml.xforms.config.XFormsConfigException;
 import org.chiba.xml.xslt.TransformerService;
 import org.chiba.xml.xslt.impl.CachingTransformerService;
 import org.chiba.xml.xslt.impl.ResourceResolver;
@@ -26,10 +31,10 @@ import com.idega.idegaweb.include.GlobalIncludeManager;
  * <p>
  * TODO gediminas Describe Type IWBundleStarter
  * </p>
- * Last modified: $Date: 2006/10/25 13:30:07 $ by $Author: gediminas $
+ * Last modified: $Date: 2006/11/02 14:40:41 $ by $Author: gediminas $
  * 
  * @author <a href="mailto:gediminas@idega.com">Gediminas Paulauskas</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class IWBundleStarter implements IWBundleStartable {
 	
@@ -42,6 +47,7 @@ public class IWBundleStarter implements IWBundleStartable {
 	public static final String TRANSFORMER_SERVICE = TransformerService.class.getName();
 
 	public static final URI XSLT_URI = URI.create("bundle://" + BUNDLE_IDENTIFIER + "/resources/xslt/html4.xsl");
+	private static final URI CHIBA_CONFIG_URI = URI.create("bundle://" + BUNDLE_IDENTIFIER + "/resources/chiba-config.xml");
 
 	/*
 	 * (non-Javadoc)
@@ -63,6 +69,19 @@ public class IWBundleStarter implements IWBundleStartable {
 		catch (TransformerException e) {
 			log.log(Level.SEVERE, "Cannot load XForms transformer stylesheet", e);
 		}
+		
+		createXFormsSessionManager(0, 0);
+		
+		try {
+			InputStream inputStream = resolver.resolve(CHIBA_CONFIG_URI).getInputStream();
+			Config.getInstance(inputStream);
+		}
+		catch (IOException e) {
+			log.warning(e.getMessage());
+		}
+		catch (XFormsConfigException e) {
+			log.log(Level.SEVERE, "Error initializing chiba config", e);
+		}
 	}
 
 	/*
@@ -72,5 +91,34 @@ public class IWBundleStarter implements IWBundleStartable {
 	 */
 	public void stop(IWBundle starterBundle) {
 		starterBundle.getApplication().removeAttribute(TRANSFORMER_SERVICE);
+		DefaultXFormsSessionManagerImpl manager = DefaultXFormsSessionManagerImpl.getInstance();
+		manager.kill();
+		manager.interrupt();
 	}
+	
+    /**
+     * factory method to create and setup an XFormsSessionManager. Overwrite this to provide your own implementation.
+     *
+     * @param wipingInterval
+     * @param timeout
+     */
+    protected void createXFormsSessionManager(int wipingInterval, int timeout) {
+        DefaultXFormsSessionManagerImpl manager = DefaultXFormsSessionManagerImpl.getInstance();
+        if(wipingInterval != 0){
+            manager.setInterval(wipingInterval);
+        }else{
+            manager.setInterval(1000 * 30);// every 30 secs as default
+        }
+
+        if (timeout != 0){
+            manager.setTimeout(timeout);
+        }else{
+            manager.setTimeout(1000 * 30); // 1 minute session lifetime
+        }
+
+        //start running the session cleanup
+
+        manager.start();
+    }
+
 }
