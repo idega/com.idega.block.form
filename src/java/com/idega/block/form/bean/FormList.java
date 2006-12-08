08fd27125c9936a0daf461668311dcb0eeef8601
Gediminas Paulauskas <gediminas@idega.com>
2006-12-08 15:38:39 +0000

    move FormList to block.form, use FormsService

---
+++ src/java/com/idega/block/form/bean/FormList.java
@@ -0,0 +1,47 @@
+package com.idega.block.form.bean;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.faces.model.SelectItem;
+import com.idega.block.form.business.FormsService;
+import com.idega.business.IBOLookup;
+import com.idega.business.IBOLookupException;
+import com.idega.idegaweb.IWApplicationContext;
+import com.idega.idegaweb.IWMainApplication;
+
+/**
+ * @author <a href="mailto:civilis@idega.com">Vytautas �ivilis</a>
+ * @version 1.0
+ * 
+ */
+public class FormList {
+	private static final Logger log = Logger.getLogger(FormList.class.getName()); 
+
+	public List<SelectItem> getForms() {
+		
+		List<SelectItem> forms = new ArrayList<SelectItem>();
+		try {
+			forms = getFormsService().listForms();
+		}
+		catch (RemoteException e) {
+			log.warning("Error getting available forms");
+		}
+		
+		return forms;
+	}
+
+	private FormsService getFormsService() {
+		FormsService service = null;
+		try {
+			IWApplicationContext iwc = IWMainApplication.getDefaultIWApplicationContext();
+			service = (FormsService) IBOLookup.getServiceInstance(iwc, FormsService.class);
+		}
+		catch (IBOLookupException e) {
+			log.severe("Could not find FormsService");
+		}
+		return service;
+	}
+	
+}
