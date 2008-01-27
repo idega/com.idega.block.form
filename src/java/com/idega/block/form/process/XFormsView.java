427f600392631d0f88fbbb78826eb1952ecd2bba
Vytautas Civilis <civilis@idega.com>
2008-01-27 13:11:07 +0000

    *** empty log message ***

--- src/java/com/idega/block/form/process/XFormsView.java
+++ src/java/com/idega/block/form/process/XFormsView.java
@@ -21,14 +21,14 @@ import com.idega.util.URIUtil;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.9 $
- *
- * Last modified: $Date: 2008/01/26 09:45:18 $ by $Author: civilis $
+ * @version $Revision: 1.10 $
+ * 
+ * Last modified: $Date: 2008/01/27 13:11:07 $ by $Author: civilis $
  */
 public class XFormsView implements View {
 
 	public static final String VIEW_TYPE = "xforms";
-	
+
 	private String viewId;
 	private boolean submitable = true;
 	private DocumentManagerFactory documentManagerFactory;
@@ -36,7 +36,7 @@ public class XFormsView implements View {
 	private Converter converter;
 	private Map<String, String> parameters;
 	private Map<String, Object> variables;
-	
+
 	public Converter getConverter() {
 		return converter;
 	}
@@ -49,7 +49,7 @@ public class XFormsView implements View {
 		form = null;
 		this.viewId = viewId;
 	}
-	
+
 	public String getViewId() {
 		return viewId;
 	}
@@ -59,9 +59,10 @@ public class XFormsView implements View {
 	}
 
 	public void setViewType(String viewType) {
-		throw new UnsupportedOperationException("XFormsView view type cannot be changed");
+		throw new UnsupportedOperationException(
+				"XFormsView view type cannot be changed");
 	}
-	
+
 	public DocumentManagerFactory getDocumentManagerFactory() {
 		return documentManagerFactory;
 	}
@@ -70,37 +71,48 @@ public class XFormsView implements View {
 			DocumentManagerFactory documentManagerFactory) {
 		this.documentManagerFactory = documentManagerFactory;
 	}
-	
+
 	public UIComponent getViewForDisplay() {
 
 		FacesContext context = FacesContext.getCurrentInstance();
 		Application application = context.getApplication();
-		
-		FormViewer formviewer = (FormViewer)application.createComponent(FormViewer.COMPONENT_TYPE);
+
+		FormViewer formviewer = (FormViewer) application
+				.createComponent(FormViewer.COMPONENT_TYPE);
 		formviewer.setXFormsDocument(getFormDocument().getXformsDocument());
-		
+
 		return formviewer;
 	}
-	
-	protected Document getFormDocument() {
-		
-		if(form != null)
-			return form;
-		
-		String formId = getViewId();
-		
-		if(formId == null || CoreConstants.EMPTY.equals(formId))
-			throw new NullPointerException("View id not set");
-		
-		try {
-			DocumentManager documentManager = getDocumentManagerFactory().newDocumentManager(FacesContext.getCurrentInstance());
-			form = documentManager.openForm(formId);
 
-			if(!isSubmitable())
-				form.setReadonly(true);
-			
+	public void setFormDocument(Document formDocument) {
+
+		form = formDocument;
+
+		if (!isSubmitable())
+			form.setReadonly(true);
+
+		setViewId(form.getId());
+	}
+
+	protected Document getFormDocument() {
+
+		if (form != null)
 			return form;
-			
+
+		String formId = getViewId();
+
+		if (formId == null || CoreConstants.EMPTY.equals(formId))
+			throw new NullPointerException("View id not set");
+
+		try {
+			DocumentManager documentManager = getDocumentManagerFactory()
+					.newDocumentManager(FacesContext.getCurrentInstance());
+			Document form = documentManager.openForm(formId);
+
+			setFormDocument(form);
+
+			return form;
+
 		} catch (RuntimeException e) {
 			throw e;
 		} catch (Exception e) {
@@ -113,41 +125,45 @@ public class XFormsView implements View {
 	}
 
 	public void setSubmitable(boolean submitable) {
-		
+
 		this.submitable = submitable;
 		getFormDocument().setReadonly(submitable);
 	}
 
 	public void populateParameters(Map<String, String> parameters) {
 		getFormDocument().getParametersManager().cleanUpdate(parameters);
-		
+
 	}
 
 	public void populateVariables(Map<String, Object> variables) {
-		getConverter().revert(variables, getFormDocument().getSubmissionInstanceElement());
+		getConverter().revert(variables,
+				getFormDocument().getSubmissionInstanceElement());
 	}
 
 	public Map<String, String> resolveParameters() {
-		
-		if(parameters != null)
+
+		if (parameters != null)
 			return parameters;
-		
-		throw new UnsupportedOperationException("Resolving parameters from form not supported yet.");
+
+		throw new UnsupportedOperationException(
+				"Resolving parameters from form not supported yet.");
 	}
 
 	public Map<String, Object> resolveVariables() {
-		
-		if(variables != null)
+
+		if (variables != null)
 			return variables;
-		
-		throw new UnsupportedOperationException("Resolving variables from form not supported yet.");
+
+		throw new UnsupportedOperationException(
+				"Resolving variables from form not supported yet.");
 	}
-	
+
 	public void setSubmission(Submission submission, Node submissionInstance) {
-		
-		String action = submission.getElement().getAttribute(FormManagerUtil.action_att);
-		
-    	parameters = new URIUtil(action).getParameters();
-    	variables = getConverter().convert(submissionInstance);
+
+		String action = submission.getElement().getAttribute(
+				FormManagerUtil.action_att);
+
+		parameters = new URIUtil(action).getParameters();
+		variables = getConverter().convert(submissionInstance);
 	}
 }
