8b71242bc50360d5370ce3589eba3932246bbdcf
Vytautas Civilis <civilis@idega.com>
2008-06-18 07:59:12 +0000

    in dev

--- src/java/com/idega/block/form/save/SaveFormSubmissionHandler.java
+++ src/java/com/idega/block/form/save/SaveFormSubmissionHandler.java
@@ -3,6 +3,7 @@ package com.idega.block.form.save;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.rmi.RemoteException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,22 +19,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
+import com.idega.block.form.presentation.FormViewer;
+import com.idega.business.IBORuntimeException;
+import com.idega.core.builder.business.BuilderService;
+import com.idega.core.builder.business.BuilderServiceFactory;
 import com.idega.documentmanager.business.PersistenceManager;
 import com.idega.documentmanager.business.XFormPersistenceType;
 import com.idega.documentmanager.util.FormManagerUtil;
+import com.idega.idegaweb.IWApplicationContext;
+import com.idega.presentation.IWContext;
 import com.idega.util.CoreConstants;
+import com.idega.util.URIUtil;
 import com.idega.util.expression.ELUtil;
 import com.idega.util.xml.XPathUtil;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  *
- * Last modified: $Date: 2008/06/17 12:18:15 $ by $Author: civilis $
+ * Last modified: $Date: 2008/06/18 07:59:12 $ by $Author: civilis $
  */
 public class SaveFormSubmissionHandler extends AbstractConnector implements SubmissionHandler {
     
 	public static final String SUBMITTED_DATA_PATH = "/files/forms/submissions";
+	
 	private PersistenceManager persistenceManager;
 	
     public Map<String, Object> submit(Submission submission, Node instance) throws XFormsException {
@@ -44,35 +53,51 @@ public class SaveFormSubmissionHandler extends AbstractConnector implements Subm
     	
     	if(formId != null && !CoreConstants.EMPTY.equals(formId)) {
     		
-    		Long fid = new Long(formId);
     		ELUtil.getInstance().autowire(this);
+    		Long fid = new Long(formId);
     		
 //    		TODO: check, if form is in firm state, if not - take it
     		
     		try {
-//        		TODO: generate form identifier if not present in the submission (some default node)
-//    			TODO: locate identifier at submission
-    			String submissionIdentifier = "foo";
+//    			trying to find form identifier from standard node (@nodeType='formIdentifier')
+    			XPathUtil u = new XPathUtil(".//*[@nodeType='formIdentifier']");
+    			Element el = (Element)u.getNode(instance);
     			
-//        		saving submission for form id
-    			Node submittedInstance = instance.cloneNode(true);
-    			XPathUtil u = new XPathUtil(".//saveFormData");
-    			Element el = (Element)u.getNode(submittedInstance);
-    			el.getParentNode().removeChild(el);
+    			final String submissionIdentifier;
     			
-    			InputStream is = getISFromXML(submittedInstance);
-    			getPersistenceManager().saveSubmittedData(fid, is, submissionIdentifier);
+    			if(el != null)
+    				submissionIdentifier = el.getTextContent();
+    			else {
 
-//        		TODO: generate link
-    			String link = "theLink";
+//            		TODO: generate form identifier if not present in the submission (some default node)
+    				submissionIdentifier = String.valueOf(System.currentTimeMillis());
+    			}
+    			
+    			System.out.println("submissionidentifier="+submissionIdentifier);
+    			
+    			InputStream is = getISFromXML(instance);
+    			Long submissionId = getPersistenceManager().saveSubmittedData(fid, is, submissionIdentifier);
+    			
+    			System.out.println("submissionId="+submissionId);
 
+//    			resolving url to formviewer and setting submission param
+    			IWContext iwc = IWContext.getCurrentInstance();
+    			BuilderService bs = getBuilderService(iwc);
+    			
+    			String url = bs.getFullPageUrlByPageType(iwc, FormViewer.formviewerPageType);
+    			final URIUtil uriUtil = new URIUtil(url);
+    			uriUtil.setParameter(FormViewer.submissionIdParam, String.valueOf(submissionId));
+    			url = uriUtil.getUri();
+    			
+    			System.out.println("url to formviewer="+url);
+    			
 //        		placing link and saved form identifier in response
     			u = new XPathUtil(".//saveFormData/formIdentifier");
     			el = (Element)u.getNode(instance);
     			el.setTextContent(submissionIdentifier);
     			u = new XPathUtil(".//saveFormData/linkToForm");
     			el = (Element)u.getNode(instance);
-    			el.setTextContent(link);
+    			el.setTextContent(url);
     			
     			is = getISFromXML(instance);
     			
@@ -115,5 +140,14 @@ public class SaveFormSubmissionHandler extends AbstractConnector implements Subm
 	@XFormPersistenceType("slide")
 	public void setPersistenceManager(PersistenceManager persistenceManager) {
 		this.persistenceManager = persistenceManager;
+	}
+	
+	protected BuilderService getBuilderService(IWApplicationContext iwc) {
+		try {
+			return BuilderServiceFactory.getBuilderService(iwc);
+			
+		} catch (RemoteException e) {
+			throw new IBORuntimeException(e);
+		}
 	}
 }
