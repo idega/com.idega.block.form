78837795a8203f75325837542fb53ba6f44de8f6
Anton Makarov <anton@idega.com>
2008-06-19 09:05:22 +0000

    *** empty log message ***

--- src/java/com/idega/block/form/process/converters/ObjectCollectionConverter.java
+++ src/java/com/idega/block/form/process/converters/ObjectCollectionConverter.java
@@ -1,10 +1,12 @@
 package com.idega.block.form.process.converters;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.chiba.xml.dom.DOMUtil;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Element;
@@ -12,71 +14,92 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import com.idega.jbpm.variables.VariableDataType;
-import com.idega.util.CoreConstants;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
 
 /**
  * @author <a href="mailto:anton@idega.com">Anton Makarov</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  *
- * Last modified: $Date: 2008/06/18 13:16:19 $ by $Author: anton $
+ * Last modified: $Date: 2008/06/19 09:05:22 $ by $Author: anton $
  */
 @Scope("singleton")
 @Service
 public class ObjectCollectionConverter implements DataConverter {
 
 	public Object convert(Element o) {
-
-	String txt = o.getTextContent();
-//		if(txt != null)
-//			txt = txt.trim();
-//		
-//		List<String> values = new ArrayList<String>();
-//		
-//		if(txt == null || txt.equals(CoreConstants.EMPTY))
-//			return values;
-//		
-//		String[] splitted = txt.split(CoreConstants.SPACE);
-//		values.addAll(Arrays.asList(splitted));
+	DOMUtil.prettyPrintDOM(o);
+	
+	@SuppressWarnings("unchecked")
+	List<Element> rowNodes = DOMUtil.getChildElements(o);
+	List<String> rowList = new ArrayList<String>();
+	
+	for(Element rowElem : rowNodes) {
+		@SuppressWarnings("unchecked")
+		List<Element> columns = DOMUtil.getChildElements(rowElem);
+		Map<String, String> columnMap = new LinkedHashMap<String, String>();
 		
-		return null;
+		for(Element columnElem : columns) {
+			columnMap.put(columnElem.getNodeName(), columnElem.getTextContent());
+		}
+		rowList.add(ObjToJSON(columnMap));
 	}
+		
+	return rowList;
+	}
+	
 	public Element revert(Object o, Element e) {
 
-//		if(!(o instanceof List))
-//			throw new IllegalArgumentException("Wrong class object provided for CollectionConverter: "+o.getClass().getName()+". Should be java.util.List");
-//		
-//		NodeList childNodes = e.getChildNodes();
-//		
-//		List<Node> childs2Remove = new ArrayList<Node>();
-//		
-//		for (int i = 0; i < childNodes.getLength(); i++) {
-//			
-//			Node child = childNodes.item(i);
-//			
-//			if(child != null && (child.getNodeType() == Node.TEXT_NODE || child.getNodeType() == Node.ELEMENT_NODE))
-//				childs2Remove.add(child);
-//		}
-//		
-//		for (Node node : childs2Remove)
-//			e.removeChild(node);
-//
-//		@SuppressWarnings("unchecked")
-//		Collection<String> values = (Collection<String>)o;
-//		
-//		StringBuilder sb = new StringBuilder();
-//		
-//		for (String value : values) {
-//
-//			if(value == null || value.trim().equals(CoreConstants.EMPTY))
-//				continue;
-//			
-//			sb.append(value);
-//			sb.append(CoreConstants.SPACE);
-//		}
-//		
-//		e.appendChild(e.getOwnerDocument().createTextNode(sb.toString().trim()));
+		if(!(o instanceof List))
+			throw new IllegalArgumentException("Wrong class object provided for ObjCollectionConverter: "+o.getClass().getName()+". Should be java.util.List");
 		
+		NodeList childNodes = e.getChildNodes();
+		
+		List<Node> childs2Remove = new ArrayList<Node>();
+		
+		for (int i = 0; i < childNodes.getLength(); i++) {
+			
+			Node child = childNodes.item(i);
+			
+			if(child != null && (child.getNodeType() == Node.TEXT_NODE || child.getNodeType() == Node.ELEMENT_NODE))
+				childs2Remove.add(child);
+		}
+		
+		for (Node node : childs2Remove)
+			e.removeChild(node);
+		
+		@SuppressWarnings("unchecked")
+		Collection<String> rowList = (Collection<String>)o;
+		
+		for(String rowStr : rowList) {
+			@SuppressWarnings("unchecked")
+			Map<String, String> columnMap = JSONToObj(rowStr);
+			
+			Element rowElem = e.getOwnerDocument().createElement("row");
+			
+			for(String column : columnMap.keySet()) {
+				Element columnEl = rowElem.getOwnerDocument().createElement(column);
+				columnEl.setTextContent(columnMap.get(column));
+				rowElem.appendChild(columnEl);
+			}
+			
+			e.appendChild(rowElem);
+		}
+		
+		DOMUtil.prettyPrintDOM(e);
 		return e;
+	}
+	
+	private String ObjToJSON(Map<String, String> obj) {
+		XStream xstream = new XStream(new JettisonMappedXmlDriver());
+		String jsonOut = xstream.toXML(obj);
+		return jsonOut;
+	}
+	
+	private Map<String, String> JSONToObj(String jsonIn) {
+		XStream xstream = new XStream(new JettisonMappedXmlDriver());
+		Map<String, String> obj = (Map<String, String>) xstream.fromXML(jsonIn);
+		return obj;
 	}
 	
 	public VariableDataType getDataType() {
