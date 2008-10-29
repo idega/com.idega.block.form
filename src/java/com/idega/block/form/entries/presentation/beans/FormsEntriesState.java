831d3f5ed863452c4a2372beddd4a135dd7f01be
Vytautas Civilis <civilis@idega.com>
2008-10-29 11:22:56 +0000

    view submission source support

--- src/java/com/idega/block/form/entries/presentation/beans/FormsEntriesState.java
+++ src/java/com/idega/block/form/entries/presentation/beans/FormsEntriesState.java
@@ -1,28 +1,34 @@
 package com.idega.block.form.entries.presentation.beans;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.faces.context.FacesContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
 
 import com.idega.block.form.entries.presentation.UIFormsEntriesViewer;
 import com.idega.documentmanager.business.PersistenceManager;
 import com.idega.documentmanager.business.Submission;
 import com.idega.documentmanager.business.XFormPersistenceType;
+import com.idega.documentmanager.util.FormManagerUtil;
+import com.idega.util.CoreConstants;
 import com.idega.util.StringUtil;
 import com.idega.util.expression.ELUtil;
 
 /**
  * 
  * @author <a href="civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  * 
- *          Last modified: $Date: 2008/10/28 13:01:07 $ by $Author: civilis $
+ *          Last modified: $Date: 2008/10/29 11:22:56 $ by $Author: civilis $
  * 
  */
 @Scope("request")
@@ -31,6 +37,8 @@ public class FormsEntriesState implements Serializable {
 
 	private static final long serialVersionUID = 7398164884900011980L;
 	private String facetDisplayed;
+	private String submissionFacetDisplayed;
+	private String submissionId;
 	private static final String submissionIdParam = "submissionId";
 
 	@Autowired
@@ -58,6 +66,16 @@ public class FormsEntriesState implements Serializable {
 
 		return UIFormsEntriesViewer.entriesFacet.equals(getFacetDisplayed());
 	}
+	
+	public boolean isSubmissionView() {
+
+		return UIFormsEntriesViewer.formSubmissionFacet.equals(getSubmissionFacetDisplayed());
+	}
+	
+	public boolean isSourceView() {
+
+		return UIFormsEntriesViewer.formSubmissionSourceFacet.equals(getSubmissionFacetDisplayed());
+	}
 
 	public boolean isFormSubmissionRendered() {
 
@@ -73,6 +91,7 @@ public class FormsEntriesState implements Serializable {
 
 		if (!StringUtil.isEmpty(submissionIdStr)) {
 			facetDisplayed = UIFormsEntriesViewer.formSubmissionFacet;
+			submissionFacetDisplayed = UIFormsEntriesViewer.formSubmissionFacet;
 		} else if (facetDisplayed == null) {
 			facetDisplayed = UIFormsEntriesViewer.entriesFacet;
 		}
@@ -87,6 +106,14 @@ public class FormsEntriesState implements Serializable {
 	public void showEntries() {
 		setFacetDisplayed(UIFormsEntriesViewer.entriesFacet);
 	}
+	
+	public void viewSource() {
+		setSubmissionFacetDisplayed(UIFormsEntriesViewer.formSubmissionSourceFacet);
+	}
+	
+	public void viewSubmission() {
+		setSubmissionFacetDisplayed(UIFormsEntriesViewer.formSubmissionFacet);
+	}
 
 	PersistenceManager getPersistenceManager() {
 
@@ -98,5 +125,51 @@ public class FormsEntriesState implements Serializable {
 
 	void setPersistenceManager(PersistenceManager persistenceManager) {
 		this.persistenceManager = persistenceManager;
+	}
+
+	public String getSubmissionFacetDisplayed() {
+		return submissionFacetDisplayed;
+	}
+
+	public void setSubmissionFacetDisplayed(String submissionFacetDisplayed) {
+		this.submissionFacetDisplayed = submissionFacetDisplayed;
+	}
+	
+	public String getSubmissionSource() {
+		
+		Long submissionId = new Long(getSubmissionId());
+		
+		Submission submission = getPersistenceManager().getSubmission(submissionId);
+		
+		Document submissionDocument = submission.getSubmissionDocument();
+		
+		String serialized;
+		
+		try {
+			serialized = FormManagerUtil.serializeDocument(submissionDocument);
+			
+		} catch (IOException e) {
+			Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Exception while serializing submission document. submission id = "+submission.getSubmissionId(), e);
+			serialized = CoreConstants.EMPTY;
+		}
+		
+		return serialized;
+	}
+
+	public String getSubmissionId() {
+
+		String submissionIdStr = (String) FacesContext.getCurrentInstance()
+		.getExternalContext().getRequestParameterMap().get(
+				submissionIdParam);
+		
+		if(!StringUtil.isEmpty(submissionIdStr)) {
+			setSubmissionId(submissionIdStr);
+		}
+		
+		return submissionId;
+	}
+
+	public void setSubmissionId(String submissionId) {
+		this.submissionId = submissionId;
 	}
 }
