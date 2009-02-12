64ea2075f1d7423335f4d39b206eee830233308f
Valdas Zemaitis <valdas@idega.com>
2009-02-12 09:08:26 +0000

    Refactored FormConverterToPDF

---
+++ src/java/com/idega/block/form/business/FormConverterToPDF.java
@@ -0,0 +1,11 @@
+package com.idega.block.form.business;
+
+public interface FormConverterToPDF {
+
+	public static final String STRING_BEAN_IDENTIFIER = "processTaskInstanceConverterToPDF";
+	
+	public String getGeneratedPDFFromXForm(String taskInstanceId, String formId, String formSubmissionId, String uploadPath, String pdfName, boolean checkExistence);
+	
+	public String getHashValueForGeneratedPDFFromXForm(String taskInstanceId, boolean checkExistence);
+	
+}
