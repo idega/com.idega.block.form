dc117df802d400d1d897d553369a6a7aa78ce109
Vytautas Civilis <civilis@idega.com>
2009-05-05 14:12:00 +0000

    refactorings due to base class changes

--- src/java/com/idega/block/form/save/SaveFormAndGeneratePdfAction.java
+++ src/java/com/idega/block/form/save/SaveFormAndGeneratePdfAction.java
@@ -11,17 +11,18 @@ import com.idega.block.pdf.business.PDFWriterProvider;
 import com.idega.chiba.web.xml.xforms.util.XFormsUtil;
 import com.idega.idegaweb.IWMainApplication;
 import com.idega.io.MediaWritable;
+import com.idega.util.StringUtil;
 import com.idega.util.URIUtil;
 
 /**
- * Handles XForm's action - saves form and generates URI to media servlet that does actual PDF rendering
- * @author <a href="mailto:valdas@idega.com>Valdas Žemaitis</a>
- * Created: 2009.02.12
- * @version $Revision: 1.1 $
- * Last modified: $Date: 2009/02/12 14:02:01 $ by $Author: valdas $
+ * Handles XForm's action - saves form and generates URI to media servlet that does actual PDF
+ * rendering
+ * 
+ * @author <a href="mailto:valdas@idega.com>Valdas Žemaitis</a> Created: 2009.02.12
+ * @version $Revision: 1.2 $ Last modified: $Date: 2009/05/05 14:12:00 $ by $Author: civilis $
  */
 public class SaveFormAndGeneratePdfAction extends SaveFormAction {
-
+	
 	private static final String saveFormAndGeneratePdf = "saveFormAndGeneratePdf";
 	
 	@Autowired
@@ -42,43 +43,55 @@ public class SaveFormAndGeneratePdfAction extends SaveFormAction {
 			final String linkExp = getXFormsAttribute("linkLocation");
 			setLinkExp(linkExp);
 			
-			final String submissionIdentifier = getXFormsAttribute("submissionIdentifier");
-			setSubmissionIdentifier(submissionIdentifier);
+			final String submissionRepresentationExp = getXFormsAttribute("submissionIdentifier");
+			setSubmissionRepresentationExp(submissionRepresentationExp);
 			
 			final String submissionIdExp = getXFormsAttribute("submissionId");
 			setSubmissionIdExp(submissionIdExp);
 			
 			final String formIdExp = getXFormsAttribute("formId");
-			final Object formIdVal = formIdExp != null
+			final String formIdStr = (String) (formIdExp != null
 			        && formIdExp.length() != 0 ? XFormsUtil
-			        .getValueFromExpression(formIdExp, this) : null;
-			setFormId(formIdVal instanceof String ? formIdVal.toString() : null);
-			        
+			        .getValueFromExpression(formIdExp, this) : null);
+			
+			if (!StringUtil.isEmpty(formIdStr)) {
+				
+				setFormId(new Long(formIdStr));
+				
+			} else {
+				
+				throw new XFormsException(
+				        "No form id resolved from the expression provided="
+				                + formIdExp);
+			}
+			
 		} else {
 			throw new XFormsException("No action specified");
 		}
 	}
-
+	
 	@Override
 	public void perform() throws XFormsException {
 		super.perform();
 		
-		String formId = getFormId();
-		Long fid = new Long(formId);
 		Instance instance = getInstance();
 		
-		final String[] submissionMeta = saveSubmission(fid, instance);
-		final String submissionUUID = submissionMeta[0];
+		saveSubmission();
+		final String submissionUUID = getSubmissionUUID();
 		
 		doRebuild(true);
 		doRecalculate(true);
 		doRevalidate(true);
 		doRefresh(true);
-			
-		URIUtil uriUtil = new URIUtil(IWMainApplication.getDefaultIWMainApplication().getMediaServletURI());
-		uriUtil.setParameter(MediaWritable.PRM_WRITABLE_CLASS, IWMainApplication.getEncryptedClassName(getWriterProvider().getPDFWriterClass()));
-		uriUtil.setParameter(getWriterProvider().getFormSubmissionUniqueIdParameterName(), submissionUUID);
-
+		
+		URIUtil uriUtil = new URIUtil(IWMainApplication
+		        .getDefaultIWMainApplication().getMediaServletURI());
+		uriUtil.setParameter(MediaWritable.PRM_WRITABLE_CLASS,
+		    IWMainApplication.getEncryptedClassName(getWriterProvider()
+		            .getPDFWriterClass()));
+		uriUtil.setParameter(getWriterProvider()
+		        .getFormSubmissionUniqueIdParameterName(), submissionUUID);
+		
 		ModelItem mi = instance.getModelItem(getLinkExp());
 		mi.setValue(uriUtil.getUri());
 	}
@@ -86,9 +99,9 @@ public class SaveFormAndGeneratePdfAction extends SaveFormAction {
 	public PDFWriterProvider getWriterProvider() {
 		return writerProvider;
 	}
-
+	
 	public void setWriterProvider(PDFWriterProvider writerProvider) {
 		this.writerProvider = writerProvider;
 	}
-
+	
 }
