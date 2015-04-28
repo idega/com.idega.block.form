60ae08447fc978d651dafa75ac0050e284ae39a1
Valdas Žemaitis <valdas@idega.com>
2015-04-28 09:15:12 +0300

    Auto added annotations

--- src/java/com/idega/block/form/IWBundleStarter.java
+++ src/java/com/idega/block/form/IWBundleStarter.java
@@ -1,9 +1,9 @@
 /**
  * $Id: IWBundleStarter.java,v 1.13 2008/11/20 16:20:54 civilis Exp $
  * Created in 2006 by gediminas
- * 
+ *
  * Copyright (C) 2000-2006 Idega Software hf. All Rights Reserved.
- * 
+ *
  * This software is the proprietary information of Idega hf. Use is subject to
  * license terms.
  */
@@ -14,7 +14,7 @@ import com.idega.idegaweb.IWBundleStartable;
 
 /**
  * Last modified: $Date: 2008/11/20 16:20:54 $ by $Author: civilis $
- * 
+ *
  * @author <a href="mailto:gediminas@idega.com">Gediminas Paulauskas</a>
  * @version $Revision: 1.13 $
  */
@@ -22,9 +22,9 @@ public class IWBundleStarter implements IWBundleStartable {
 
 	public static final String BUNDLE_IDENTIFIER = "com.idega.block.form";
 
-	public void start(IWBundle starterBundle) {
-	}
+	@Override
+	public void start(IWBundle starterBundle) {}
 
-	public void stop(IWBundle starterBundle) {
-	}
+	@Override
+	public void stop(IWBundle starterBundle) {}
 }
